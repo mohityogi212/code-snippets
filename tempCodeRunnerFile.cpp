8527e205fd7382a2df4dc618caed995bfc85440e
Mohit Yogi <83246282+mohityogi212@users.noreply.github.com>
2022-10-09 23:30:43 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+//any no of empty lines can be left there will be no effect on main program
+
+void sum() {
+    int a;      //ek fxn me bnaya hua variable local vaar hota hai
+    int glo = 69;
+}
+int main() {
+//Built-in Data types
+/* int 
+float 
+double 
+char 
+bool
+*/
+// int a = 4 ;  
+// int b = 7 ;
+// int c = 9 ;
+// int sum = a + b + c ; 
+
+// float pi = 3.141; // D N o L P
+
+// double num = 1.0720000000 ; // D N o  H  P 
+
+/* char part = 'a';
+sum();
+cout<<glo;
+*/
+
+
+
+
+    // cout << "sum is\n" << sum << "value of pi is \n"<< pi << "value of double num is : \n" << num <<"value of char is ";
+   
+
+    return 0;
+}
